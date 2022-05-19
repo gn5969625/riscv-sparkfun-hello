1c3d3c6a707eaa5357974db7a849a67c43f5c3b6
Kevin Shen <gn5969625@gmail.com>
2022-05-19 05:38:24 -0700

    Add global test code

--- bootmain.c
+++ bootmain.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #define UART_TXFIFO_OFF	0
 #define UART_REG_BASE	0x10013000
-
+uint32_t cnt = 0;
 void main(void)
 {
 	char dbg_msg[] = "Hello world in c!\r\n";
 	volatile uint32_t *uart_buf = (uint32_t *) (UART_REG_BASE + UART_TXFIFO_OFF);
 	uint32_t msg_len = sizeof(dbg_msg);
 	uint32_t i = 0;
+	char test_msg[] = "Enter loop in c!\r\n";
 	for (i = 0;i < msg_len; i++) {
 		while((int32_t) *uart_buf < 0);
-		*uart_buf = dbg_msg[i];
+		*uart_buf = test_msg[i];
+	}
+	while (cnt++ < 5) {
+		for (i = 0;i < msg_len; i++) {
+			while((int32_t) *uart_buf < 0);
+			*uart_buf = dbg_msg[i];
+		}
 	}
 	while(1); // never return
 }
