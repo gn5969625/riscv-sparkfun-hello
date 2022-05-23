9cd1b75cd3f6f42165d7e595b0b4a37350fc434a
Kevin Shen <gn5969625@gmail.com>
2022-05-23 10:37:31 +0800

    Debug for global variable

--- bootmain.c
+++ bootmain.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #define UART_TXFIFO_OFF	0
 #define UART_REG_BASE	0x10013000
+//#define DBG_GV 1
+#ifdef DBG_GV
 uint32_t cnt = 0;
+#endif
 void main(void)
 {
 	char dbg_msg[] = "Hello world in c!\r\n";
 	volatile uint32_t *uart_buf = (uint32_t *) (UART_REG_BASE + UART_TXFIFO_OFF);
 	uint32_t msg_len = sizeof(dbg_msg);
 	uint32_t i = 0;
+#ifndef DBG_GV
+	uint32_t cnt = 0;
+#endif
+#ifdef DBG_GV
 	char test_msg[] = "Enter loop in c!\r\n";
 	for (i = 0;i < msg_len; i++) {
 		while((int32_t) *uart_buf < 0);
 		*uart_buf = test_msg[i];
 	}
+#endif
 	while (cnt++ < 5) {
 		for (i = 0;i < msg_len; i++) {
 			while((int32_t) *uart_buf < 0);
